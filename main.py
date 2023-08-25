1f5e94ceb8e923af42fcac9d64aec4a04c25f7eb
neko88 <84291442+neko88@users.noreply.github.com>
2023-08-25 13:47:44 -0700

    added horizontal collision function

--- main.py
+++ main.py
@@ -110,12 +110,12 @@ class Player(pygame.sprite.Sprite):
         self.y_vel = -self.GRAVITY * 8      ## negative for jumping up (0,0)TLC so negative subtracts "closer" to y=0
         self.animation_count = 0
         self.jump_count += 1
-        if self.jump_count == 1:
+        if self.jump_count == 1:        ## if we are jumping, turn off GRAVITY
             self.fall_count = 0
 
 ## 13a. Create function for fall
     def fall(self, fps):
-        self.y_vel += min(1, (self.fall_count/fps) * self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count/fps) * self.GRAVITY)      ## **GRAVITY is added when fall_count > 0!
         self.fall_count += 1
 
 ## 16a. Create landed function wrt vertical collision
@@ -134,6 +134,13 @@ class Player(pygame.sprite.Sprite):
         sprite_sheet = "idle"   ## the individual anims of each sprite sheet has an 'action
         if self.x_vel != 0:
             sprite_sheet = "run"
+        elif self.y_vel > self.GRAVITY * 2:
+            sprite_sheet = "fall"
+        elif self.y_vel < 0:
+            if self.jump_count == 1:
+                sprite_sheet = "jump"
+            elif self.jump_count == 2:
+                sprite_sheet = "double_jump"
 
         sprite_sheet_name = sprite_sheet + "_" + self.direction ## anim + dir it is facing
         sprites = self.SPRITES[sprite_sheet_name]
@@ -152,10 +159,13 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
         self.update_sprite()
 
+    ## scene is : scene + offset_x so...
+    ## player move to left -> offset negative -> push scene right
+    ## player move to right -> offset positive -> push scene left
     ## visual for the player movement
-    def draw(self, window):
+    def draw(self, window, offset_x):
         ## self.sprite = self.SPRITES["idle_" + self.direction][0] // example
-        window.blit(self.sprite, (self.rect.x, self.rect.y))
+        window.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 
 ## 14. Create a class Object
@@ -172,13 +182,14 @@ class Object(pygame.sprite.Sprite):
     def update(self):
         self.rect = self.sprite.get_rect(topLeft=(self.rect.x, self.rect.y))
 
-    def draw(self, window):
-        window.blit(self.sprite, (self.rect.x, self.rect.y))
+    def draw(self, window, offset_x):
+        window.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 
 
 ## 15. Create a class Block which inherits from Object
 ## Blocks will be size x size
+## TIP: spawn space of block sizes by x * block_size
 class Block(Object):
     def __init__(self, x, y, sprite_x, sprite_y, width, height):
         super().__init__(x, y, width, height)  ## from Object
@@ -206,21 +217,21 @@ def get_background(bg_name):
 
 
 ## 6. Define the draw method
-def draw(window, background, bg_img, player, objects):
+def draw(window, background, bg_img, player, objects, offset_x):
     for tile in background:
         ## blit() displays tile on screen -> screen.blit(image, (100, 100))
         window.blit(bg_img, tuple(tile))
         ## 8a. execute the drawing of the player in the window
-        player.draw(window)
+        player.draw(window, offset_x)
 
         for obj in objects:
-            obj.draw(window)
+            obj.draw(window, offset_x)
 
         pygame.display.update()
 
 
 ## 16. Create a functin to handle collisions from top/bottom on player
-def handle_vertical_collision(player, objects, dy):
+def collide_vertical(player, objects, dy):
     collided_objects = []
     player.mask = pygame.mask.from_surface(player.sprite)
     for obj in objects:
@@ -239,6 +250,23 @@ def handle_vertical_collision(player, objects, dy):
 
     return collided_objects
 
+## 20. Create a horizontal collision function
+## Note, horizontal collisions should be checked first before vertical collision
+## Move player by dx, check if H.Collision, if yes, move dx back.
+## This allows checking before they accidentally 'move' into the block
+def collide_horizontal(player, objects, dx):
+    player.move(dx, 0)
+    player.update() ## update the current pos of the player
+    collided_object = None
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            collided_object = obj
+            break
+    player.move(-dx,0)
+    player.update()
+    return collided_object
+
+
 # 9. Create method to handle movement from keypress
 def handle_move(player, objects):
     keys = pygame.key.get_pressed() ## gets keys being pressed
@@ -248,7 +276,7 @@ def handle_move(player, objects):
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
 
-    handle_vertical_collision(player, objects, player.y_vel)
+    collide_vertical(player, objects, player.y_vel)
 
 ## 2. Define the main method and pass window
 def main(window):
@@ -264,6 +292,14 @@ def main(window):
     floor = [ Block(i * block_size, HEIGHT - block_size, 96, 0, block_size, block_size)
               for i in range( -WIDTH // block_size, (WIDTH*2) // block_size ) ]
 
+    ## 18. Create a container of 'objects' - in includes the prev floor created before
+    objects = [*floor,
+               Block(0, HEIGHT - (block_size * 2), 96, 0, block_size, block_size),
+               Block(block_size * 3, HEIGHT - (block_size * 3), 96, 64, block_size, block_size)] ## multiply 2 to put higher on screen
+
+    offset_x = 0
+    scroll_area_width = 200
+
     ## 4. Setup for running &stopping the program
     while run:
         ## 4a. set up the clock
@@ -277,9 +313,17 @@ def main(window):
                     player.jump()
 
         player.loop(FPS)
-        handle_move(player, floor)
+        handle_move(player, objects)
         ## 6. write the execution for draw method
-        draw(window, background, bg_img, player, floor)
+        draw(window, background, bg_img, player, objects, offset_x)
+
+        ## 17. Writing the script for scrolling backgroun as player moves
+        ## if the player reaches a pixel within the scroll_area_width, move the bg
+        ## the offset is the amount that will scroll the scene
+        if (((player.rect.right - offset_x) >= (WIDTH - scroll_area_width) and (player.x_vel > 0)) or
+                ((player.rect.left - offset_x) >= (WIDTH - scroll_area_width) and (player.x_vel > 0))):
+            offset_x += player.x_vel ## player movement ADDS to offset
+
 
     pygame.quit()
     quit()
